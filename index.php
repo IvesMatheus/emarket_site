997b8d3053a277ab11f268ce543432b55a5319f6
IvesMatheus <ives_matheus@hotmail.com>
2016-08-22 12:40:35 -0400

    Telas iniciais

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>MyMarket</title>
+        <title>meuMercadinho</title>
         <meta charset="utf-8"/>
         <link rel="stylesheet" href="_css/index.css">
     </head>
     <body>
         <header id="cabecalho">
-            <div id="img"></div>
+            <div id="img_logo">
+                <h1>meuMercadinho</h1>
+                <h5>O mercadinho de vizinhança online</h5>
+            </div>
             <nav id="menu">
-                <!--<ul>
-                    <li>
-                        <a href="http://localhost/SD/">HOME</a>
-                    </li>
-                </ul>-->
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li>LOGIN</li>
+                </ul>
             </nav>
         </header>
-        <div id="principal"></div>
-        <div id="rodape"></div>
+        <div id="main">
+            <div id="sobre">
+                <h3>Conheça meuMercadinho</h3>
+                <h3 class="subtitle">O que é</h3>
+                <p><span class="termo_destaque">meuMercadinho</span> é um sistema na qual você pode montar sua própria loja online para vender os produtos de seu mercado!</p>
+            </div>
+            <div id="lateral">
+                teste
+            </div>
+        </div>
     </body>
 </html>
